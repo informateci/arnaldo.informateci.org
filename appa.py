e2d022f80f95220cc54a77ddcd150a53803aff95
autoscatto <4utoscatto@gmail.com>
2013-09-19 19:45:03 +0200

    la bella e la fista.

--- appa.py
+++ appa.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 from flask import Flask, redirect, render_template, url_for, send_from_directory, request
 from arnaldo import sproloquio, accendi_il_cervello
-#from motorino_d_avviamento import accendi_il_cervello
-PATH='/home/httpd/arnaldo.informateci.org'
+PATH='/home/autoscatto/prototipi/git/arnaldo.informateci.org'
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 import os
 os.chdir(PATH + '/arnaldo')
 accendi_il_cervello()
 os.chdir(PATH)
-
-#count_proverbia = []
-#def contaggio(arg):
-#    sleep(600)
-#    count_proverbia = []
-#
-#from threading import Thread
-#
-#t = Thread(target = contaggio)
-#t.start()
 
 @app.route("/proverbia")
 def proverbia():
@@ -36,8 +24,6 @@ def proverbia():
 
 @app.route("/boobs")
 def boobs():
-    #if arna == None:
-    #    return "None"
     return render_template("boobsplease.html", link = sproloquio.boobs())
 
 @app.route("/attardati")
