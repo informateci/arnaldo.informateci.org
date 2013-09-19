3505564c10031886c0754e3c188a7a4033ecd3d7
autoscatto <4utoscatto@gmail.com>
2013-09-19 17:09:29 +0200

    +Template -Parate

--- appa.py
+++ appa.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
 from flask import Flask, redirect, render_template, url_for, send_from_directory, request
 from arnaldo import sproloquio, accendi_il_cervello
 #from motorino_d_avviamento import accendi_il_cervello
+PATH='/home/httpd/arnaldo.informateci.org'
 
 app = Flask(__name__)
 import os
-os.chdir('/home/httpd/arnaldo.informateci.org/arnaldo')
+os.chdir('./arnaldo')
 accendi_il_cervello()
-os.chdir('/home/httpd/arnaldo.informateci.org')
+os.chdir(PATH)
 
 #count_proverbia = []
 #def contaggio(arg):
@@ -20,54 +22,38 @@ os.chdir('/home/httpd/arnaldo.informateci.org')
 
 @app.route("/proverbia")
 def proverbia():
-    import sqlite3
-    conn = sqlite3.connect('/home/httpd/arnaldo.informateci.org/database.db')
-    c = conn.cursor()
     if request.method == 'GET':
-        ip = str(request.remote_addr)
-        #if ip not in count_proverbia:
-        #    count_proverbia.append(ip)
-        try:
-            i = (request.args['id'],)
-            c.execute("SELECT proverbio FROM proverbia WHERE id = ?", i)
-            p = c.fetchone()[0].replace('\\\'','\'')
-            conn.close()
-            return render_template("proverbia.html", proverbio = p, users = "0", id = str(i[0]))
-        except Exception as e:
-            try:
-                p = sproloquio.proverbia()
-                c.execute("INSERT INTO proverbia(proverbio) VALUES (?)", (p.replace('\'','\\\''),))
-                conn.commit()
-                conn.close()
-                i = c.lastrowid
-                import urllib
-                return render_template("proverbia.html", proverbio = p, id = str(i), users = "0")
-            except Exception as e:
-                return str(e.args) + '<br/>'+str(e) + ' ' + p
-            #i = c.lastrowid
-            #conn.commit()
-            #return str(i)
-            #import urllib
-            #return render_template("proverbia.html", proverbio = p, id = str(i), urlprov = urllib.quote(p.encode('utf8')), users = str(len(count_proverbia)))
-    conn.close()
+        i = request.args.get('id', False)
+        print i
+        if i:
+            p=sproloquio.proverbiabyid(i)
+            pid=i
+        else:
+            p,pid=sproloquio.proverbiaandid()
+        return render_template("proverbia.html", proverbio = p, id = pid)
 
 @app.route("/boobs")
 def boobs():
-    if arna == None:
-        return "None"
+    #if arna == None:
+    #    return "None"
     return render_template("boobsplease.html", link = sproloquio.boobs())
 
 @app.route("/attardati")
 def attardati():
-    return render_template("proverbia.html", proverbio = sproloquio.attardati())
+    return render_template("proverbia.html", proverbio = sproloquio.attardati(), ricarica="attardati")
 
-@app.route("/ANAL")
-def ANAL():
-    return render_template("proverbia.html", proverbio = sproloquio.ANAL())
+@app.route("/anal")
+def anal():
+    return render_template("proverbia.html", proverbio = sproloquio.ANAL(), ricarica="anal")
 
-@app.route("/<filename>")
+
+@app.route("/static/<filename>")
 def arnafile(filename):
-    return send_from_directory('/home/httpd/arnaldo.informateci.org/resources', filename)
+    return send_from_directory(os.path.join(PATH,'resources'), filename)
+
+@app.route("/")
+def ANAL():
+    return render_template("base.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
